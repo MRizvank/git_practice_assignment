ce176c81beef409f8155362079a6d299841822ab
notlegal96 <shenm09@gmail.com>
2022-10-20 00:30:44 +0530

    alpha_01

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,10 @@
 //Check if the Number is a palindrome
-var str = "Tiwari" ;
+let str = "dad"
 var bag = "" ;
 
 
-for(var  i = str.length-1; i<0 ; i++) {
-  bag += str[p];
+for(var  i = str.length-1; i>=0 ; i--) {
+  bag += str[i];
 }
 if(str == bag){
   console.log("Yes");
