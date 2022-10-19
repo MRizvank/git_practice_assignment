8a60b41d93806cfc314ecc2a48aa4c8f9dcc3959
Rizvan <rizvankurungattil@gmail.com>
2022-10-19 23:54:41 +0530

    prime and palindrome

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+//Check if the Number is a palindrome
+var str = "Tiwari" ;
+var bag = "" ;
+
+
+for(var  i = str.length-1; i<0 ; i++) {
+  bag += str[p];
+}
+if(str == bag){
+  console.log("Yes");
+}else{
+  console.log("No");
+}
+//please find the error
---
+++ prime.js
@@ -0,0 +1,12 @@
+let a = 13;
+let count=0;
+    for(let i=0; i>=a; i--){
+        if(a%i==0){
+            a++;
+        }
+    }if (count!=2){
+        console.log("Yes");
+        
+    }else{
+        console.log("No");
+    } 
